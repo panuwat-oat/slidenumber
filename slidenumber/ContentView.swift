3228231a7fe617f465bb7f3ec74bd07ae25a07ac
Oath <140141614+panuwat-oat@users.noreply.github.com>
2023-11-20 15:20:37 +0700

    Update ContentView.swift

--- slidenumber/ContentView.swift
+++ slidenumber/ContentView.swift
@@ -1,24 +1,83 @@
-//
-//  ContentView.swift
-//  slidenumber
-//
-//  Created by Oath-M2 on 20/11/2566 BE.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numbers = (1...15).shuffled() + [0] // 0 represents the empty space
+    @State private var movesCount = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Slide Number Puzzle")
+                .font(.largeTitle)
+            
+            Grid(numbers: $numbers, movesCount: $movesCount)
+            
+            Button("New Game") {
+                numbers = (1...15).shuffled() + [0]
+                movesCount = 0
+            }
+            
+            Text("Moves: \(movesCount)")
+                .font(.headline)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct Grid: View {
+    @Binding var numbers: [Int]
+    @Binding var movesCount: Int
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 10) {
+            ForEach(numbers.indices, id: \.self) { index in
+                NumberView(number: numbers[index])
+                    .onTapGesture {
+                        moveNumber(at: index)
+                    }
+                    .animation(.linear, value: numbers) // Animate the change in numbers array
+                
+            }
+        }
+        .padding()
+    }
+    
+    func moveNumber(at index: Int) {
+        let row = index / 4
+        let column = index % 4
+        let emptyIndex = numbers.firstIndex(of: 0)!
+        let emptyRow = emptyIndex / 4
+        let emptyColumn = emptyIndex % 4
+        
+        // Check if the empty space is adjacent to the tapped number
+        if (row == emptyRow && abs(column - emptyColumn) == 1) || (column == emptyColumn && abs(row - emptyRow) == 1) {
+            numbers.swapAt(index, emptyIndex)
+            movesCount += 1
+        }
+    }
 }
+
+struct NumberView: View {
+    let number: Int
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(number == 0 ? .clear : .orange)
+                .frame(width: 70, height: 70)
+                .shadow(radius: number == 0 ? 0 : 5)
+            
+            if number != 0 {
+                Text("\(number)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+

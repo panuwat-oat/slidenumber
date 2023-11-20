e8fb28e7518e1cb676c3658ef4db4569aa8078fa
Oath <140141614+panuwat-oat@users.noreply.github.com>
2023-11-20 13:58:41 +0700

    Initial Commit

---
+++ slidenumber/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  slidenumber
+//
+//  Created by Oath-M2 on 20/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ slidenumber/slidenumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  slidenumberApp.swift
+//  slidenumber
+//
+//  Created by Oath-M2 on 20/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct slidenumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
